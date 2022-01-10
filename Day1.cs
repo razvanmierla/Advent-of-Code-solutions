43a5b1bd410381c9651ac1c2013ff478258c3e5d
Razvan <razvan.mierla13@gmail.com>
2022-01-11 01:40:20 +0200

    Day 1 puzzle solution

---
+++ Day1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    internal class Day1
+    {
+
+        public static int GetResult(string inputPath)
+        {
+            string[] lines = File.ReadAllLines(inputPath);
+
+            int result = -1;
+            int previousSum = -1;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (i + 2 == lines.Length)
+                {
+                    break;
+                }
+                int sum = Int32.Parse(lines[i]) + Int32.Parse(lines[i + 1]) + Int32.Parse(lines[i + 2]);
+                if (previousSum < sum)
+                {
+                    result++;
+                }
+                previousSum = sum;
+            }
+
+            return result;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@ class TestClass
 {
     static void Main()
     {
-        
+        string dirPath = $"{Directory.GetCurrentDirectory()}\\Puzzle_Inputs";
+        string day1_inputPath = $"{dirPath}\\input_day1.txt";
+
+        var Day1_result = Day1.GetResult(day1_inputPath);
     }
 }
